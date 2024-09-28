1789e88b8bd41ff39d0f9d4445d400dc381e502d
long <nguyenthanhlong@morenergy.net>
2024-09-28 21:24:13 +0200

    feat: creat component, install echarts

---
+++ src/app/components/sankey-chart/sankey-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sankey-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './sankey-chart.component.html',
+  styleUrl: './sankey-chart.component.scss'
+})
+export class SankeyChartComponent {
+
+}
