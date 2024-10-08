74997fb4ab53a26e72b5232b5441777198ee27cc
long <nguyenthanhlong@morenergy.net>
2024-10-08 14:03:31 +0200

    feat: snackbar feedback

--- src/app/pages/docs-page/docs-page.component.ts
+++ src/app/pages/docs-page/docs-page.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserManualComponent } from '../../components/user-manual/user-manual.component';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-docs-page',
   standalone: true,
   imports: [
     MatButtonModule, MatIconModule, MatMenuModule,
-    UserManualComponent, NavbarComponent],
+    UserManualComponent, NavbarComponent, MatSnackBarModule],
   templateUrl: './docs-page.component.html',
   styleUrl: './docs-page.component.scss'
 })
 export class DocsPageComponent {
+  private _snackBar = inject(MatSnackBar);
 
 
   clearLocalStorage() {
     localStorage.clear();
+    this._snackBar.open('Local storage cleared', 'Dismiss', {
+      duration: 2000,
+    })
   }
 }
