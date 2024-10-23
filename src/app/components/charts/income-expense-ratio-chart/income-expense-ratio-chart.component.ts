7160aab9f45e7ae3b1735b9db2f4561a6cf3d0e5
long <nguyenthanhlong@morenergy.net>
2024-10-24 00:26:16 +0200

    refactor: avoid redundant code

--- src/app/components/charts/income-expense-ratio-chart/income-expense-ratio-chart.component.ts
+++ src/app/components/charts/income-expense-ratio-chart/income-expense-ratio-chart.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { EChartsOption } from 'echarts';
+import { Component, effect, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { EChartsOption, SeriesOption } from 'echarts';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 import { DataService } from '../../../services/data.service';
 import { ColorService } from '../../../services/color.service';
@@ -13,7 +13,7 @@ import { ColorService } from '../../../services/color.service';
     provideEcharts(),
   ],
   templateUrl: './income-expense-ratio-chart.component.html',
-  styleUrl: './income-expense-ratio-chart.component.scss'
+  styleUrls: ['./income-expense-ratio-chart.component.scss']
 })
 export class IncomeExpenseRatioChartComponent implements OnChanges {
   @Input() totalIncome: number = 0;
@@ -22,135 +22,102 @@ export class IncomeExpenseRatioChartComponent implements OnChanges {
   dataService = inject(DataService)
   colorService = inject(ColorService)
 
+  barWidth: string = '10%'
+  chartOption: EChartsOption = this.getBaseChartOptions();
+
   constructor() {
     effect(() => {
-      this.scaleChart()
-    })
+      this.updateChart();
+    });
   }
 
-  barWidth: string = '10%'
-  chartOption: EChartsOption = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: { type: 'shadow' },
-      backgroundColor: this.colorService.isDarkMode() ? this.colorService.darkBackgroundSecondary : this.colorService.lightBackgroundPrimary,
-      textStyle: {
-        color: this.colorService.isDarkMode() ? this.colorService.darkTextPrimary : this.colorService.lightTextPrimary,
-      },
-      formatter: (params: any) => {
-        const visibleParams = params.filter((item: any) => item.seriesName !== 'scale')
-        let tooltip = ''
-        visibleParams.forEach((item: any) => {
-          tooltip += `
-          <div style="display: flex; justify-content: space-between; align-items: center;">
-            <div style="flex: 1; display: flex; align-items: center;">
-              ${item.marker} ${item.seriesName}:
-            </div>
-            &nbsp;
-            <div style="flex: 1; text-align: right;">
-              <strong>${item.value}</strong>
-            </div>
-          </div>`;
-        });
-        return tooltip
-      },
-    },
-    grid: {
-      left: '2%',
-      right: '5%',
-      bottom: '0%',
-      top: '-12%',
-      containLabel: true
-    },
-    xAxis: {
-      type: 'value',
-      boundaryGap: [0, 0.01],
-      splitLine: { show: false },
-    },
-    yAxis: {
-      type: 'category',
-      data: [''],
-      axisLabel: {
-        show: false
-      }
-    },
-  };
-
-  mergeOption: EChartsOption
-  
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes && changes['totalIncome'] || changes['totalExpense']) {
+    if (changes['totalIncome'] || changes['totalExpense']) {
       this.updateChart();
     }
   }
 
-
-
-  updateChart() {
-    this.mergeOption = 
-    {
-      series: [
-        {
-          name: 'Total Income',
-          type: 'bar',
-          data: [this.totalIncome],
-          barWidth: this.barWidth,
-          itemStyle: {
-            color: this.colorService.isDarkMode() ? '#A0CA7E' : 'rgb(157, 202, 127)',  // Green for income
-            borderRadius: [0, 100, 100, 0]
-          }
+  getBaseChartOptions(): EChartsOption {
+    return {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'shadow' },
+        backgroundColor: this.colorService.isDarkMode() ? this.colorService.darkBackgroundSecondary : this.colorService.lightBackgroundPrimary,
+        textStyle: {
+          color: this.colorService.isDarkMode() ? this.colorService.darkTextPrimary : this.colorService.lightTextPrimary,
         },
-        {
-          name: 'Total Expense',
-          type: 'bar',
-          data: [this.totalExpense],
-          barWidth: this.barWidth,
-          itemStyle: {
-            color: this.colorService.isDarkMode() ? '#E07A6A' : 'rgb(222, 110, 106)',  // Red for expense
-            borderRadius: [0, 100, 100, 0]
-          }
-        },
-      ]
-    }
-    this.chartOption = { ...this.chartOption, ...this.mergeOption }
+        formatter: (params: any) => this.getCustomTooltip(params),
+      },
+      grid: {
+        left: '2%',
+        right: '5%',
+        bottom: '0%',
+        top: '-12%',
+        containLabel: true,
+      },
+      xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01],
+        splitLine: { show: false },
+      },
+      yAxis: {
+        type: 'category',
+        data: [''],
+        axisLabel: { show: false },
+      },
+    };
   }
 
+  getCustomTooltip(params: any): string {
+    const visibleParams = params.filter((item: any) => item.seriesName !== 'scale');
+    return visibleParams.map((item: any) => `
+      <div style="display: flex; justify-content: space-between; align-items: center;">
+        <div style="flex: 1; display: flex; align-items: center;">
+          ${item.marker} ${item.seriesName}:
+        </div>
+        &nbsp;
+        <div style="flex: 1; text-align: right;">
+          <strong>${item.value.toLocaleString()}</strong>
+        </div>
+      </div>`).join('');
+  }
 
-  scaleChart() {
-    this.mergeOption = {
-      series: [
-        {
-          name: 'Total Income',
-          type: 'bar',
-          data: [this.totalIncome],
-          barWidth: this.barWidth,
-          itemStyle: {
-            color: this.colorService.isDarkMode() ? '#A0CA7E' : 'rgb(157, 202, 127)',  // Green for income
-            borderRadius: [0, 100, 100, 0]
-            
-          }
+  updateChart(): void {
+    this.chartOption = {
+      ...this.getBaseChartOptions(),
+      series: this.getSeriesOptions(),
+    };
+  }
+
+  getSeriesOptions(): SeriesOption[] {
+    return [
+      {
+        name: 'Total Income',
+        type: 'bar',
+        data: [this.totalIncome],
+        barWidth: this.barWidth,
+        itemStyle: {
+          color: this.colorService.isDarkMode() ? '#A0CA7E' : 'rgb(157, 202, 127)', // Green for income
+          borderRadius: [0, 100, 100, 0],
         },
-        {
-          name: 'Total Expense',
-          type: 'bar',
-          data: [this.totalExpense],
-          barWidth: this.barWidth,
-          itemStyle: {
-            color: this.colorService.isDarkMode() ? '#E07A6A' : 'rgb(222, 110, 106)',  // Red for expense
-            borderRadius: [0, 100, 100, 0]
-          }
+      },
+      {
+        name: 'Total Expense',
+        type: 'bar',
+        data: [this.totalExpense],
+        barWidth: this.barWidth,
+        itemStyle: {
+          color: this.colorService.isDarkMode() ? '#E07A6A' : 'rgb(222, 110, 106)', // Red for expense
+          borderRadius: [0, 100, 100, 0],
         },
-        {
-          name: 'scale',
-          type: 'bar',
-          data: [this.dataService.incomeExpenseScaleValue()],
-          barWidth: '0.1%',
-          itemStyle: {
-            color: 'rgba(0,0,0,0)',
-          },
-        },
-      ]
-    }
-    this.chartOption = { ...this.chartOption, ...this.mergeOption }
+      },
+      {
+        name: 'scale',
+        type: 'bar',
+        data: [this.dataService.incomeExpenseScaleValue()],
+        barWidth: '0.1%',
+        itemStyle: { color: 'rgba(0,0,0,0)' },
+      },
+    ];
   }
 }
