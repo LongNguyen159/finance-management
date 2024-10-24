6341a264a7301b30610bd6699d1748051cbd21f8
long <nguyenthanhlong@morenergy.net>
2024-10-25 01:13:51 +0200

    feat: update page

--- src/app/pages/whats-new-page/whats-new-page.component.ts
+++ src/app/pages/whats-new-page/whats-new-page.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ColorService } from '../../services/color.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-whats-new-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './whats-new-page.component.html',
   styleUrl: './whats-new-page.component.scss'
 })
 export class WhatsNewPageComponent {
+  colorService = inject(ColorService)
 
 }
