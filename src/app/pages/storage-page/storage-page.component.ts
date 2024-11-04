1b8cafad1efa76275965540a73274d19e494989a
long <nguyenthanhlong@morenergy.net>
2024-11-04 17:23:44 +0100

    style: icon color for mat-tab

--- src/app/pages/storage-page/storage-page.component.ts
+++ src/app/pages/storage-page/storage-page.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StorageManagerComponent } from '../../components/storage-manager/storage-manager.component';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FixCostInputComponent } from "../../components/fix-cost-input/fix-cost-input.component";
+import { CommonModule } from '@angular/common';
+import { ColorService } from '../../services/color.service';
 
 @Component({
   selector: 'app-storage-page',
   standalone: true,
-  imports: [StorageManagerComponent, NavbarComponent, MatTabsModule, MatButtonModule, MatIconModule, FixCostInputComponent],
+  imports: [StorageManagerComponent, NavbarComponent, MatTabsModule, MatButtonModule, MatIconModule, FixCostInputComponent,
+    CommonModule
+  ],
   templateUrl: './storage-page.component.html',
   styleUrl: './storage-page.component.scss'
 })
 export class StoragePageComponent {
-
+  colorService = inject(ColorService)
 }
