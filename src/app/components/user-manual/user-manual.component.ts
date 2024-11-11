3f4c0116aeb218fbd532a209af7c9e7c618f748c
long <nguyenthanhlong@morenergy.net>
2024-11-11 17:57:54 +0100

    chore: update docs

--- src/app/components/user-manual/user-manual.component.ts
+++ src/app/components/user-manual/user-manual.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ColorService } from '../../services/color.service';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-manual',
   standalone: true,
-  imports: [CommonModule, MatDividerModule],
+  imports: [CommonModule, MatDividerModule, MatIconModule],
   templateUrl: './user-manual.component.html',
   styleUrl: './user-manual.component.scss'
 })
