2c7b8ca93e301e39a44e9ec8be2f507576a9c2a1
long <nguyenthanhlong@morenergy.net>
2024-11-25 13:36:37 +0100

    host gh pages

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
-      devTools: false, // disable devTools for production
+      devTools: true, // disable devTools for production
     },
     frame: true,         // Show the window frame (this allows you to drag and resize the window)
     resizable: true,     // Allow resizing the window (optional)
